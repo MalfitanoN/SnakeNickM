3c865e53407c5d9741e87b2dbf9f63cf28b4e776
Nicholas Malfitano <nicholas1malfitano@gmail.com>
2014-10-08 15:04:21 -0700

    color, snake size and height

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -3,6 +3,12 @@ var Snake;
 var context;
 var screenWidth;
 var screenHeight;
+var snakeLength
+var snakeSize
+
+
+gameInitialize();
+gameDraw();
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen")
@@ -21,5 +27,19 @@ function gameLoop() {
 
 function gameDraw() {
     context.fillStyle = "rgb(52, 135, 194)";  
-    context.fillRect();
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitalize() {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
 }
